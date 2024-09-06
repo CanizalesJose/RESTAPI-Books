2184e7aafda7ea646bd494dbdf972c23a782e4ed
CanizalesJose <jose1605m@gmail.com>
2024-09-06 16:04:07 -0700

    Implementada conexión con MySQL

--- src/connection/db.js
+++ src/connection/db.js
@@ -1,18 +1,32 @@
-var { Sequelize } = require('sequelize')
-var sequelize = new Sequelize('printondemand', 'root', '1234', {
-    host: 'localhost',
-    dialect : 'mysql',
-    logging : 'false'
-});
+const mysql = require('mysql2/promise');
 
-var connection = async() =>{
-    try{
-        await sequelize.authenticate();
-        console.log('Connection to DB established');
-    } catch(err){
-        console.log('Connection to DB failed');
-        process.exit(1);
+class DB {
+    constructor() {
+        this.pool = mysql.createPool({
+            host: "localhost",
+            user: "root",
+            password: "1234",
+            database: "booksCenter",
+            waitForConnections: true,
+            connectionLimit: 10,   // Número máximo de conexiones simultáneas
+            queueLimit: 0          // Número máximo de conexiones en espera (0 para ilimitado)
+        });
     }
-};
 
-module.exports = {sequelize, connection};
+    async query(sql, params = []) {
+        try {
+            const [results, fields] = await this.pool.execute(sql, params);
+            return results;
+        } catch (err) {
+            console.error('Error en la consulta:', err);
+            throw err;
+        }
+    }
+
+    async closePool() {
+        await this.pool.end();
+        console.log('Pool de conexiones cerrado');
+    }
+}
+
+module.exports = new DB();
