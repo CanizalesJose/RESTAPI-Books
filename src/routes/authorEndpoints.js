9f2bbe70ae37056e9d5cb26c0dbb420f447f7145
CanizalesJose <jose1605m@gmail.com>
2024-09-08 18:38:35 -0700

    Ajustadas rutas de API en Authors

--- src/routes/authorEndpoints.js
+++ src/routes/authorEndpoints.js
@@ -19,10 +19,15 @@ router.get('/info', function(req, res){
     res.status(200).json({message: 'This section contains a list of authors for the books.'})
 });
 
-router.get('/get/:authorId', async function(req, res) {
+router.get('/searchByid', async function(req, res) {
+    console.log(`${req.method} method on route "${req.path}"`);
+    res.status(200).json([{message: 'This route let you find an author by his ID'}]);
+});
+
+router.get('/searchByid/:authorId', async function(req, res) {
     console.log(`${req.method} method on route "${req.path}"`);
     try {
-        const authorQuery = await db.query('SELECT * from author where authorId = ?', [req.params.authorId]);
+        const authorQuery = await db.query('SELECT * from authors where authorId = ?', [req.params.authorId]);
         console.log(authorQuery);
         if (authorQuery.length==0){
             res.status(204).json(null);
@@ -35,10 +40,29 @@ router.get('/get/:authorId', async function(req, res) {
     }
 });
 
+router.get('/searchByname', async function(req, res) {
+    console.log(`${req.method} method on route "${req.path}"`);
+    res.status(200).json([{message: 'This route let you search for an author by name, it returns as many as it finds'}]);
+});
+
+router.get('/searchByname/:authorName', async function(req, res) {
+    console.log(`${req.method} method on route "${req.path}"`);
+    try {
+        const authorsQuery = await db.query('SELECT * FROM authors WHERE authorName LIKE ?', [`%${req.params.authorName}%`]);
+        if (authorsQuery.length==0)
+            res.status(204).json(null);
+        else
+        res.status(200).json(authorsQuery);
+    } catch (ex) {
+        console.error(ex.message);
+        res.status(500).json([{message: 'An unexpected error ocurred'}]);
+    }
+});
+
 router.get('/getall', async function(req, res) {
     console.log(`${req.method} method on route "${req.path}"`);
     try {
-        const authorsQuery = await db.query('SELECT * FROM author');
+        const authorsQuery = await db.query('SELECT * FROM authors');
         if (authorsQuery.length==0)
             res.status(204).json(null);
         else
