cb06f70fa0279c0ac58e65b3e034c788cdf425ea
CanizalesJose <jose1605m@gmail.com>
2024-09-21 03:49:29 -0700

    Agregado uso de estados

--- src/routes/users.js
+++ src/routes/users.js
@@ -29,7 +29,7 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    
+    res.status(200).json({sessionToken: 'asdasdasd'});
 });
 
 module.exports = router;
