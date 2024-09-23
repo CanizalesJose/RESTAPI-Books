a04871c8ce26df7cfa719812d4e2268ae6fba0d8
CanizalesJose <jose1605m@gmail.com>
2024-09-23 16:13:00 -0700

    Actualizado funcionamiento de Autores

--- src/models/authorDAO.js
+++ src/models/authorDAO.js
@@ -1,6 +1,7 @@
 const db = require('../connection/db');
 
 class authorModel{
+    // Recibe un id, comprueba y regresa un autor, genera un error en caso de falla
     static async find(id){
         const mainSqlQuery = 'SELECT id, fullName, nationality from Authors WHERE id = ?';
         try {
@@ -8,14 +9,15 @@ class authorModel{
                 throw new Error("Faltan parametros");
             if (id.length == 0 || id.length > 15)
                 throw new Error("El id no es valido");
-            const autor = await db.query(mainSqlQuery, [id]);
-            if (!autor)
+            const results = await db.query(mainSqlQuery, [id]);
+            if (!results)
                 throw new Error("Error en la consulta");
-            return autor;
+            return results;
         } catch (error) {
             throw error;
         }
     }
+    // Regresa todos los autores, en caso de falla genera un error
     static async findAll(){
         const mainSqlQuery = 'SELECT id, fullName, nationality from Authors';
         try {
@@ -23,6 +25,87 @@ class authorModel{
             if (!results)
                 throw new Error("Error en la consulta");
             return results;
+        } catch (error) {
+            throw error;
+        }
+    }
+    // Registra un usuario, lanza error si falla
+    static async register(newId=null, newFullname=null, newNationality=null){
+        const mainSqlQuery = 'INSERT Authors(id, fullname, nationality) VALUES (?, ?, ?)';
+        try {
+            if (!newId || !newFullname || !newNationality)
+                throw new Error("Faltan parametros");
+            if (newId.length == 0 || newId.length > 15)
+                throw new Error("El Id no cumple los requisitos de dato");
+            if (newFullname.length == 0 || newFullname.length > 100)
+                throw new Error("El nombre no cumple los requisitos de dato");
+            if (newNationality.length == 0 || newNationality > 50)
+                throw new Error("La nacionalidad no cumple los requisitos de dato");
+            const result = await this.find(newId);
+            if (!result)
+                throw new Error("Error en la consulta");
+            if (result.length > 0)
+                throw new Error("El registro ya existe");
+            await db.query(mainSqlQuery, [newId, newFullname, newNationality]);
+            return 0;
+        } catch (error) {
+            throw error;
+        }
+    }
+    // Actualiza un dato de un autor en base a los datos enviados, lanza error si falla
+    static async update(id=undefined, newFullname=undefined, newNationality=undefined){
+        const nameSqlQuery = 'UPDATE Authors SET fullname = ? WHERE id =?';
+        const nationalitySqlQuery = 'UPDATE Authors SET nationality = ? WHERE id = ?';
+        try {
+            var changeName = false; 
+            var changeNation = false;
+            // Comprobar id
+            if (!id)
+                throw new Error("Falta el parametro id");
+            if (id.length == 0 || id.length > 15)
+                throw new Error("El id no cumple con los requisitos de dato");
+            // Comprobar nombre
+            if (newFullname){
+                if (newFullname.length == 0 || newFullname.length > 100)
+                    throw new Error("El nombre no cumple con los requisitos de dato");
+                changeName = true;
+            }
+            // Comprobar nacionalidad
+            if (newNationality){
+                if (newNationality.length == 0 || newNationality.length > 50)
+                    throw new Error("La nacionalidad no cumple con los requisitos de dato");
+                changeNation = true;
+            }
+            // Comprobra que al menos un parametro se haya enviado
+            if (changeName == false && changeNation == false)
+                throw new Error("Al menos el nombre o la nacionalidad se debe mandar como parametro");
+            // Revisar si existe el registro a actualizar
+            const result = this.find(id);
+            if (!result)
+                throw new Error("Error en la consulta");
+            if (result.length == 0)
+                throw new Error("El registro no existe");
+            // Si no fallos en parametros y el registro existe, entonces actualiza
+            if (changeName == true)
+                await db.query(nameSqlQuery, [newFullname, id]);
+            if (changeNation == true)
+                await db.query(nationalitySqlQuery, [newNationality, id]);
+            return 0;
+        } catch (error) {
+            throw error;
+        }
+    }
+    static async delete(id=undefined){
+        const mainSqlQuery = 'DELETE FROM Authors WHERE id = ?';
+        try {
+            if (!id)
+                throw new Error("Falta el parametro id");
+            const result = this.find(id);
+            if (!result)
+                throw new Error("Error en la consulta");
+            if (result.length == 0)
+                throw new Error("El registro no existe");
+            await db.query(mainSqlQuery, [id]);
         } catch (error) {
             throw error;
         }
--- src/routes/authors.js
+++ src/routes/authors.js
@@ -1,113 +1,56 @@
-var router = require('express').Router();
-var db = require('../connection/db');
-/*
-    Ejemplo de uso de consultas a base de datos
+const router = require('express').Router();
+const authorDAO = require('../models/authorDAO');
+const { authenticateToken, authorizeRoles, printPath } = require('../utils');
+// Ruta protegida para buscar un autor
+router.get('/find', authenticateToken, authorizeRoles(['admin']), async (req, res) => {
+    const {id} = req.body;
     try {
-        const listAuthors = await db.query('SELECT * from author where authorId = ?', [req.params.authorId]);
-        res.status(200).json(listAuthors);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Error en la consulta a base de datos');
-    }
- */
-
-router.get('/', function(req, res) {
-    res.status(200).json({message: 'Connected to Books, Resource: Author'})
-});
-
-router.get('/info', function(req, res){
-    res.status(200).json({message: 'Este apartado permite buscar autores en base a ciertos parámetros.'})
-});
-
-router.get('/getById', async function(req, res) {
-    console.log(`${req.method} on route "${req.path}"`);
-    res.status(200).json([{message: 'Esta ruta permite encontrar autores en base a su ID'}]);
-});
-
-// Ruta para encontrar autores con su ID
-router.get('/getByid/:authorId', async function(req, res) {
-    console.log(`${req.method} on route "${req.path}"`);
-    try {
-        const authorQuery = await db.query('SELECT * from authors where authorId = ?', [req.params.authorId]);
-        console.log(authorQuery);
-        if (authorQuery.length==0){
-            res.status(204).json(null);
-        }else{
-            res.status(200).json(authorQuery);
-        }
-    } catch (ex) {
-        console.error(ex.message);
-        res.status(500).json([{message: 'An unexpected error ocurred'}]);
+        const result = await authorDAO.find(id);
+        return res.status(200).json(result);
+    } catch (error) {
+        return res.status(400).json({message: error.message});
     }
 });
-
-router.get('/getByName', async function(req, res) {
-    console.log(`${req.method} on route "${req.path}"`);
-    res.status(200).json([{message: 'Esta ruta permite encontrar autores cuyo nombre incluya una cadena, regresa tantos como encuentre'}]);
-});
-
-// Ruta para encontrar autores cuyo nombre contenga un String
-router.get('/getByName/:authorName', async function(req, res) {
-    console.log(`${req.method} on route "${req.path}"`);
+// Ruta protegida para regresar todos los autores
+router.get('/findAll', authenticateToken, authorizeRoles(['admin']), async (req, res) => {
     try {
-        const authorsQuery = await db.query('SELECT * FROM authors WHERE authorName LIKE ?', [`%${req.params.authorName}%`]);
-        if (authorsQuery.length==0)
-            res.status(204).send(null);
-        else
-        res.status(200).json(authorsQuery);
-    } catch (ex) {
-        console.error(ex.message);
-        res.status(500).json([{message: 'An unexpected error ocurred'}]);
+        const results = await authorDAO.findAll();
+        return res.status(200).json(results);
+    } catch (error) {
+        return res.status(400).json({message: error.message});
     }
 });
-
-router.get('/getByCategory', async function(req, res) {
-    console.log(`${req.method} on route "${req.path}"`);
-    res.json([{message: "Esta ruta permite buscar un autor que haya escrito libros de una categoría especifica"}]);
-});
-
-// Ruta para encontrar autores que tengan libros con una categoría especifica
-router.get('/getByCategory/:bookCategory', async function(req, res) {
-    console.log(`${req.method} method on route "${req.path}"`);
+// Ruta protegida para registrar un nuevo autor
+router.post('/register', authenticateToken, authorizeRoles(['admin']), async (req, res) => {
+    const {newId, newFullname, newNationality} = req.body;
     try {
-        const authorsQuery = await db.query('select authorId, authorName, authorNationality from authors inner join books on authors.authorid=books.bookauthor inner join categories on books.bookcategory=categories.categoryid where categoryid = ? group by authorid;', [req.params.bookCategory]);
-        if (authorsQuery.length==0)
-            res.status(204).send(null);
-        else
-            res.status(200).json(authorsQuery);
-    } catch (ex) {
-        console.error(ex.message);
-        res.status(500).json([{message: "An unexpected error ocurred"}]);
+        await authorDAO.register(newId, newFullname, newNationality);
+        return res.status(200).json({message: 'Autor registrado'});
+    } catch (error) {
+        return res.status(400).json({message: error.message});
     }
 });
-
-// Regresa todos los autores en base de datos
-router.get('/getall', async function(req, res) {
-    console.log(`${req.method} on route "${req.path}"`);
+// Ruta para actualizar un registro
+router.patch('/update', authenticateToken, authorizeRoles(['admin']), async (req, res) => {
+    const {id, newFullname, newNationality} = req.body;
     try {
-        const authorsQuery = await db.query('SELECT * FROM authors');
-        if (authorsQuery.length==0)
-            res.status(204).json(null);
-        else
-            res.status(200).json(authorsQuery);
-    } catch (ex) {
-        console.error(ex.message);
-        res.status(500).json([{message: 'An unexpected error ocurred'}]);
+        await authorDAO.update(id, newFullname, newNationality);
+        return res.status(200).json({message: 'Autor actualizado'});
+    } catch (error) {
+        return res.status(400).json({message: error.message});
     }
 });
-
-router.post('/postAuthor', async function(req, res){
-    console.log(`${req.method} on route "${req.path}"`);
+// Ruta para eliminar un registro
+router.delete('/delete', authenticateToken, authorizeRoles(['admin']), async (req, res) => {
+    const {id} = req.body;
     try {
-        const newAuthor = JSON.parse(req.body);
-        console.log(newAuthor);
-        req.status(200).send("Done!");
-    } catch (ex) {
-        console.error(ex.message);
-        res.status(500).json([{message: "An unexpected error ocurred"}]);
+        await authorDAO.delete(id);
+        return res.status(200).json({message: 'Autor eliminado'});
+    } catch (error) {
+        return res.status(400).json({message: error.message});
     }
 });
-
+// Ruta por defecto cuando se hace una solicitud a una ruta no definida
 router.use((req, res, next) =>{
     res.status(404).json([{message: `The requested route with method ${req.method} does not exists`}]);
 });
