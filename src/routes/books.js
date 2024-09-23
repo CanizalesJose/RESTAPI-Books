a808a085a03e086abde010bede4585177c865d51
CanizalesJose <jose1605m@gmail.com>
2024-09-22 21:58:02 -0700

    Endpoints actualizados

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,24 +1,30 @@
 var router = require('express').Router();
 const db = require('../connection/db');
+const bookDAO = require('../models/bookDAO');
+const {authenticateToken, authorizeRoles, printPath} = require('../utils');
 
-router.get('/getBook', function(req, res) {
-    console.log(`${req.method} on route "${req.path}"`);
-    res.status(200).json([{message: 'Esta ruta permite buscar un libro por su ID'}]);
+router.get('/findAll', authenticateToken, authorizeRoles(['admin']), async (req, res) => {
+    printPath(req.path, req.method);
+    try {
+        const allBooks = await bookDAO.findAll();
+        res.status(200).json(allBooks);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({message: error.message});
+    }
 });
 
-router.get('/getBook/:bookId', async function(req, res) {
-    console.log(`${req.method} method on route "${req.path}"`);
-    try{
-        const bookQuery = await db.query('SELECT * FROM books where bookId = ?', [req.params.bookId]);
-
-        if (bookQuery.length == 0)
-            res.status(204).json(null);
-        else
-            res.status(200).json(bookQuery);
-    } catch(ex){
-        console.error(ex.message);
-        res.status(500).json([{message: 'An unexpected error ocurred'}]);
+router.post('/register',authenticateToken, authorizeRoles(['admin']), async (req, res) => {
+    printPath(req.path, req.method);
+    const {newId, newTitle, newIsbn, newAuthor, newPublisher, newPublishYear, newCategory, newCopies, newImageUrl} = req.body;
+    // Si no hay url de imagen, usa la predeterminada
+    if (!newImageUrl)
+        newImageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT58P55blSKZmf2_LdBoU7jETl6OiB2sjYy9A&s';
+    // Si falta al menos un otro argumento no se puede registrar
+    if (!newId || !newTitle || !newIsbn || !newAuthor || !newPublisher || !newPublishYear || !newCategory || !newCopies){
+        res.status(400).json({message: 'Faltan parametros'});
     }
+    
 });
 
 router.use((req, res, next) =>{
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
-const userModel = require('../models/userModel');
+const userDAO = require('../models/userDAO');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const { authenticateToken, authorizeRoles, printPath } = require('../utils');
+const { authenticateToken, authorizeRoles, printPath, encryptText } = require('../utils');
 
-// Ruta base para comprobar conexión
-router.get('/', async (req, res) => {
-    printPath(req.path, req.method);
-
-    res.status(200).json({message: 'Access to users on BooksCenter API'});
-});
 // Recibe en el body un username y password, regresa 201 y el token generado si el usuario existe
 router.post('/login', async (req, res) => {
     printPath(req.path, req.method);
@@ -17,28 +11,29 @@ router.post('/login', async (req, res) => {
     try {
         const {loginUsername, loginPassword} = req.body;
 
-        const user = userModel.findUser(loginUsername);
+        const user = await userDAO.findUser(loginUsername);
 
         if (!user || user.length == 0 || !bcrypt.compareSync(loginPassword, user[0]['userpassword'])){
             return res.status(401).json({message: 'Credenciales inválidas'});
         }
 
-        const token = jwt.sign({id: user[0]['username']}, process.env.SECRET_KEY, {expiresIn: '15m'});
+        const token = jwt.sign({username: user[0]['username'], usertype: user[0]['usertype']}, process.env.SECRET_KEY, {expiresIn: '20m'});
 
-        res.status(201).json({token: token});
+        res.status(201).json({token: token, username: user[0]['username'], usertype: user[0]['usertype']});
     } catch (error) {
         console.error(error.message);
         res.status(500).json({message: error.message});
     }
 });
-// Recibe en el body los parametros para crear un usuario
+// Recibe en el body un usuario y contraseña en texto plano
 router.post('/register', async (req, res) => {
     printPath(req.path, req.method);
 
     try {
-        const {newUsername, newhashedPassword, newUsertype} = req.body;
-        const valid = await userModel.registerUser(newUsername, newhashedPassword, newUsertype);
-        if (valid == 1)
+        const {newUsername, newPassword, newUsertype} = req.body;
+        const newHashedPassword = await encryptText(newPassword);
+        const valid = await userDAO.registerUser(newUsername, newHashedPassword, newUsertype);
+        if (valid == 0)
             res.status(201).json({message: 'Usuario registrado correctamente'});
         else
             res.status(409).json({message: 'El usuario ya existe'});
@@ -48,11 +43,11 @@ router.post('/register', async (req, res) => {
     }
 });
 // Ruta protegida, regresa todos los usuarios autenticados
-router.get('/findAll', authenticateToken, authorizeRoles(['1']), async (req, res) => {
+router.get('/findAll', authenticateToken, authorizeRoles(['admin']), async (req, res) => {
     printPath(req.path, req.method);
 
     try {
-        const users = await userModel.findAllUsers();
+        const users = await userDAO.findAllUsers();
         res.status(200).json(users);
             
     } catch (error) {
@@ -61,22 +56,72 @@ router.get('/findAll', authenticateToken, authorizeRoles(['1']), async (req, res
     }
 });
 // Ruta protegida para modificar usuarios
-router.post('/updateUser', authenticateToken, authorizeRoles(['1']), async (req, res) =>{
+router.post('/update', authenticateToken, authorizeRoles(['admin']), async (req, res) =>{
     printPath(req.path, req.method);
 
     try {
-        const {currentUsername, newHashedPassword, newUserType} = req.body;
-
-        if (newHashedPassword){
-            // Código para cambiar contraseña
+        const {changedUser, newPassword, newUsertype} = req.body;
+        
+        var changes = 'Datos cambiados: ';
+        // Revisar body de la petición
+        if (!changedUser){
+            return res.status(400).json({message: 'El usuario actualizado no se ha recibido'});
         }
-        if (newUserType){
-            // Código para cambiar tipo de usuario
+        if (!newPassword && !newUsertype){
+            return res.status(400).json({message: 'Se debe mandar por lo menos una nueva contraseña o un nuevo tipo de usuario'});
         }
-        res.status(200).json({message: 'Usuario actualizado correctamente'});
+        // Comprobar requisitos de datos
+        if (newPassword && (newPassword.length > 100 || newPassword.length == 0)){
+            return res.status(400).json({message: 'La contraseña no puede estar vacía ni ser mayor a 100 caracteres'});
+        }
+        if (newUsertype && (newUsertype != "admin" && newUsertype != "client")){
+            console.log(newUsertype);
+            return res.status(400).json({message: 'El tipo de usuario debe ser "admin o "client"'});
+        }
+        // Actualizar datos
+        var changed = 1;
+        if (newPassword){
+            // Actualizar contraseña
+            changed = await userDAO.updatePassword(encryptText(newPassword), changedUser);
+            if (changed == 1)
+                changes += 'hubo un error actualizando contraseña '
+            else
+                changes += 'contraseña ';
+            changed = 1;
+        }
+        if (newUsertype){
+            // Actualizar tipo de usuario
+            changed = await userDAO.updateUsertype(newUsertype, changedUser);
+            if (changed == 1)
+                changes += 'hubo un error actualizando el tipo de usuario '
+            else
+                changes += 'usertype';
+        }
+        res.status(201).json({message: `Usuario actualizado correctamente. ${changes}`});
     } catch (error) {
         console.error(error.message);
         res.status(500).json({message: error.message});
     }
+});
+// Ruta protegida para eliminar usuarios
+router.post('/delete', authenticateToken, authorizeRoles(['admin']), async (req, res) => {
+    printPath(req.path, req.method);
+    const {deletedUsername} = req.body;
+    if (!deletedUsername)
+        res.status(400).json({message: 'Se debe enviar un nombre de usuario'});
+    try {
+        const valid = await userDAO.deleteUser(deletedUsername);
+        if (valid == 1)
+            res.status(500).json({message: 'Ocurrió un error al eliminar el usuario'});
+        if (valid == 0)
+            res.status(200).json({message: 'Usuario eliminado con éxito'});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({message: error.message});
+    }
+});
+// Middleware para gestionar rutas no existentes
+router.use((req, res, next) =>{
+    res.status(404).json([{message: `The requested route with method ${req.method} does not exists`}]);
 });
 module.exports = router;
